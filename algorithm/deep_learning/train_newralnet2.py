c65402b6fe257b091e80872f66dcc96fe074419e
kakeX <kake777@hotmail.com>
2021-10-20 09:45:58 +0900

    master

--- algorithm/deep_learning/train_newralnet2.py
+++ algorithm/deep_learning/train_newralnet2.py
@@ -5,13 +5,17 @@ from two_layer_net import TwoLayerNet
 
 (x_train, t_train), (x_test, t_test) = load_mnist(normalize=True, one_hot_label=True)
 
-train_loss_list = []
-
 # ハイパーパラメータ
 iters_num = 10000
 train_size = x_train.shape[0]
 batch_size = 100
 learning_late = 0.1
+
+train_loss_list = []
+train_acc_list = []
+test_acc_list = []
+# 1エポック辺りの繰り返し数
+iter_per_epoch = max(train_size / batch_size, 1)
 
 network = TwoLayerNet(input_size=784, hidden_size=50, output_size=10)
 
@@ -37,9 +41,21 @@ for i in range(iters_num):
     loss = network.loss(x_batch, t_batch)
     train_loss_list.append(loss)
 
+    # 1エポックごとに認識精度を計算
+    if i % iter_per_epoch == 0:
+        train_acc = network.accuracy(x_train, t_train)
+        test_acc = network.accuracy(x_test, t_test)
+        train_acc_list.append(train_acc)
+        test_acc_list.append(test_acc)
+        print("train acc, test acc | " + str(train_acc) + "," + str(test_acc))
+
 # グラフの描画
-x = np.arange(len(train_loss_list))
-plt.plot(x, train_loss_list)
-plt.xlabel("iteration")
-plt.ylabel("loss")
+markers = {'train': 'o', 'test': 's'}
+x = np.arange(len(train_acc_list))
+plt.plot(x, train_acc_list, label='train acc')
+plt.plot(x, test_acc_list, label='test acc', linestyle='--')
+plt.xlabel("epochs")
+plt.ylabel("accuracy")
+plt.ylim(0, 1.0)
+plt.legend(loc='lower right')
 plt.show()
