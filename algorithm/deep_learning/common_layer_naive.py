810b12032323853598783c706cc2f88c979be34c
kakeX <kake777@hotmail.com>
2021-11-10 16:57:30 +0900

    master

---
+++ algorithm/deep_learning/common_layer_naive.py
@@ -0,0 +1,33 @@
+# 乗算レイヤ
+class MulLayer:
+    def __init__(self):
+        self.x = None
+        self.y = None
+    
+    def forward(self, x, y):
+        self.x = x
+        self.y = y
+        out = x * y
+    
+        return out
+    
+    def backword(self, dout):
+        # Xとyをひっくり返す
+        dx = dout * self.y
+        dy = dout * self.x
+
+        return dx, dy
+
+# 加算レイヤ
+class AddLayer:
+    def __init__(self):
+        pass
+    
+    def forward(self, x, y):
+        out = x + y
+        return out
+    
+    def backword(self, dout):
+        dx = dout * 1
+        dy = dout * 1
+        return dx, dy
